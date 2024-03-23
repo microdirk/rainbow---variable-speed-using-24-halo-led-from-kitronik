265d1fd1cb3992fe5b23e6cb298ec136c9f27c07
microdirk <164635705+microdirk@users.noreply.github.com>
2024-03-23 19:09:13 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -42,15 +42,13 @@ let DelayMax = SpeedMax
 let Display = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 Display.showRainbow(1, 360)
 basic.forever(function () {
-    while (true) {
-        if (Direction2 == 1) {
-            Display.rotate(1)
-            Display.show()
-        } else if (Direction2 == -1) {
-            Display.rotate(-1)
-            Display.show()
-        }
-        Delay = DelayMax - Speed
-        basic.pause(Delay)
+    if (Direction2 == 1) {
+        Display.rotate(1)
+        Display.show()
+    } else if (Direction2 == -1) {
+        Display.rotate(-1)
+        Display.show()
     }
+    Delay = DelayMax - Speed
+    basic.pause(Delay)
 })
